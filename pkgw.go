a0be17c74c49efec65946a92a812899e87cce912
Kostiantyn Masliuk <1pkg@protonmail.com>
2020-01-18 12:47:56 +0100

    add visit implementation for package walker

--- pkgw.go
+++ pkgw.go
@@ -9,11 +9,14 @@ import (
 	"regexp"
 )
 
+// Pkgw defines package walker struct that is capable of
+// walking through all package's structs and apply action on them
 type Pkgw struct {
 	fset *token.FileSet
 	pkg  *ast.Package
 }
 
+// NewPackageWalker creates instance of Pkgw
 func NewPackageWalker(fpkg string) (*Pkgw, error) {
 	fset := token.NewFileSet()
 	var all = func(os.FileInfo) bool { return true }
@@ -30,5 +33,40 @@ func NewPackageWalker(fpkg string) (*Pkgw, error) {
 	return &Pkgw{fset: fset, pkg: pkg}, nil
 }
 
+// Visit is Pkgw implementation of Walker Visit
+// it goes through all struct decls inside the package
+// and applies action if struct name matches regexp
 func (pkgw Pkgw) Visit(reg *regexp.Regexp, apply Apply) {
+	// go through all files inside the package
+	for _, file := range pkgw.pkg.Files {
+		// go through all declarations inside a file
+		for _, decl := range file.Decls {
+			// check if decl is gendecl
+			gdecl, ok := decl.(*ast.GenDecl)
+			if !ok {
+				continue
+			}
+
+			// go through all gendecl specs
+			for _, spec := range gdecl.Specs {
+				// check if spec is typespec
+				tspec, ok := spec.(*ast.TypeSpec)
+				if !ok {
+					continue
+				}
+
+				// check if typespec type is structype
+				st, ok := tspec.Type.(*ast.StructType)
+				if !ok {
+					continue
+				}
+
+				// check if struct name matches regexp
+				// in case it does then apply action
+				if reg.MatchString(tspec.Name.Name) {
+					apply(st)
+				}
+			}
+		}
+	}
 }
