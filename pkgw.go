3b24cd4fcf2e0bccbd1dd2e5973ca910d799ef8a
Kostiantyn Masliuk <alfa0dude@gmail.com>
2020-01-18 13:00:57 +0100

    add package parser abstracion for package walker

--- pkgw.go
+++ pkgw.go
@@ -16,16 +16,27 @@ type Pkgw struct {
 	pkg  *ast.Package
 }
 
-// NewPackageWalker creates instance of Pkgw
-func NewPackageWalker(fpkg string) (*Pkgw, error) {
-	fset := token.NewFileSet()
+// Pkgp is package parser func abstraction for package parsing process
+type Pkgp func(string) (map[string]*ast.Package, *token.FileSet, error)
+
+// DefaultPkgp implements Pkgp abstraction and
+// executes parser.ParseDir to collect pakages, fileset and err
+var DefaultPkgp = func(pkg string) (pkgs map[string]*ast.Package, fset *token.FileSet, err error) {
+	fset = token.NewFileSet()
 	var all = func(os.FileInfo) bool { return true }
 	var mode = parser.Mode(0)
-	pkgs, err := parser.ParseDir(fset, fpkg, all, mode)
+	pkgs, err = parser.ParseDir(fset, pkg, all, mode)
+	return
+}
+
+// NewPackageWalker creates instance of Pkgw
+func NewPackageWalker(fpkg string, pkgp Pkgp) (*Pkgw, error) {
+	// use parser to collect pakages, fileset and err
+	pkgs, fset, err := pkgp(fpkg)
 	if err != nil {
 		return nil, err
 	}
-
+	// check if pakages list has desired package
 	pkg, ok := pkgs[fpkg]
 	if !ok {
 		return nil, fmt.Errorf("package %s wasn't found", fpkg)
@@ -46,7 +57,6 @@ func (pkgw Pkgw) Visit(reg *regexp.Regexp, apply Apply) {
 			if !ok {
 				continue
 			}
-
 			// go through all gendecl specs
 			for _, spec := range gdecl.Specs {
 				// check if spec is typespec
@@ -54,13 +64,11 @@ func (pkgw Pkgw) Visit(reg *regexp.Regexp, apply Apply) {
 				if !ok {
 					continue
 				}
-
 				// check if typespec type is structype
 				st, ok := tspec.Type.(*ast.StructType)
 				if !ok {
 					continue
 				}
-
 				// check if struct name matches regexp
 				// in case it does then apply action
 				if reg.MatchString(tspec.Name.Name) {
