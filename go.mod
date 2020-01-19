1d88a9eee661549d7b74fddd8daba1648cca25b9
Kostiantyn Masliuk <alfa0dude@gmail.com>
2020-01-19 16:59:55 +0100

    update strategy signature plus pkgw, pkgp to simplify complex parsing fix all depeneded things

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.4.0
+	golang.org/x/tools v0.0.0-20200117220505-0cba7a3a9ee9
 	gopkg.in/yaml.v2 v2.2.7 // indirect
 )
--- pkgw_test.go
+++ pkgw_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"context"
 	"errors"
-	"go/ast"
+	"go/token"
+	"go/types"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -24,56 +26,45 @@ func TestNewPackageWalker(t *testing.T) {
 			err:  errors.New("error package test error"),
 		},
 		{
-			name: "empty package name, new package walker should return error",
-			fpkg: "",
-			pkgp: MockPkgp(map[string]*ast.Package{"foo": nil, "bar": nil}).Parse,
-			pkgw: nil,
-			err:  errors.New("package `` wasn't found"),
-		},
-		{
-			name: "package parser returns nil map, new package walker should return error",
-			fpkg: "foobar",
-			pkgp: MockPkgp(nil).Parse,
-			pkgw: nil,
-			err:  errors.New("package `foobar` wasn't found"),
-		},
-		{
-			name: "package parser returns empty map, new package walker should return error",
-			fpkg: "foobar",
-			pkgp: MockPkgp(make(map[string]*ast.Package)).Parse,
-			pkgw: nil,
-			err:  errors.New("package `foobar` wasn't found"),
-		},
-		{
 			name: "package name wasn't found, new package walker should return error",
 			fpkg: "foobar",
-			pkgp: MockPkgp(map[string]*ast.Package{"foo": nil, "bar": nil}).Parse,
+			pkgp: NotFoundPkgp{}.Parse,
 			pkgw: nil,
 			err:  errors.New("package `foobar` wasn't found"),
 		},
 		{
-			name: "single nil package was found, new package walker should return correct package walker",
+			name: "package parser returns nil type package, new package walker should return error",
 			fpkg: "foobar",
-			pkgp: MockPkgp(map[string]*ast.Package{"foobar": nil}).Parse,
-			pkgw: &Pkgw{},
-			err:  nil,
+			pkgp: MockPkgp{fset: token.NewFileSet()}.Parse,
+			pkgw: nil,
+			err:  errors.New("package `foobar` wasn't found"),
 		},
 		{
-			name: "package was found in the map, new package walker should return correct package walker",
+			name: "package parser returns nil fset, new package walker should return error",
 			fpkg: "foobar",
-			pkgp: MockPkgp(map[string]*ast.Package{
-				"foo":    &ast.Package{Name: "foo"},
-				"bar":    &ast.Package{Name: "bar"},
-				"foobar": &ast.Package{Name: "foobar"},
-			}).Parse,
-			pkgw: &Pkgw{pkg: &ast.Package{Name: "foobar"}},
-			err:  nil,
+			pkgp: MockPkgp{pkg: types.NewPackage("/", "foobar")}.Parse,
+			pkgw: nil,
+			err:  errors.New("package `foobar` wasn't found"),
+		},
+		{
+			name: "package was found, new package walker should return correct package walker",
+			fpkg: "foobar",
+			pkgp: MockPkgp{
+				pkg:  types.NewPackage("/", "foobar"),
+				fset: token.NewFileSet(),
+			}.Parse,
+			pkgw: &Pkgw{
+				pkg:  types.NewPackage("/", "foobar"),
+				fset: token.NewFileSet(),
+			},
+			err: nil,
 		},
 	}
 	t.Run("new package walker should return correct results for all cases", func(t *testing.T) {
 		for _, tcase := range table {
 			t.Run(tcase.name, func(t *testing.T) {
-				pkgw, err := NewPackageWalker(tcase.fpkg, tcase.pkgp)
+				ctx := context.Background()
+				pkgw, err := NewPackageWalker(ctx, tcase.fpkg, tcase.pkgp)
 				assert.Equal(t, tcase.pkgw, pkgw)
 				assert.Equal(t, tcase.err, err)
 			})
