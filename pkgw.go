319057eedd7c31c3a1f0427f0d21c1904cd004eb
Kostiantyn Masliuk <1pkg@protonmail.com>
2020-01-20 20:59:55 +0100

    extend walker type to support recursive tree walk

--- pkgw.go
+++ pkgw.go
@@ -8,14 +8,16 @@ import (
 	"regexp"
 )
 
-// Walker is interface that describes hierarchical walker that
-// applies some strategy on ast.StructType
+// Walker defines hierarchical walker abstraction
+// that applies some strategy to tree structures
 type Walker interface {
-	Visit(context.Context, *regexp.Regexp, Strategy)
+	VisitTop(context.Context, *regexp.Regexp, Strategy) // only top level of the tree
+	VisitRec(context.Context, *regexp.Regexp, Strategy) // all levels of the tree
 }
 
-// Pkgw defines package walker struct that is capable of
-// walking through all package's structs and apply strategy on them
+// Pkgw defines package walker struct implementation
+// that is capable of walking through all package's structs
+// and apply specified strategy to them
 type Pkgw struct {
 	pkg  *types.Package
 	fset *token.FileSet
@@ -23,7 +25,7 @@ type Pkgw struct {
 
 // NewPackageWalker creates instance of Pkgw
 func NewPackageWalker(ctx context.Context, spkg string, pkgp Pkgp) (*Pkgw, error) {
-	// use parser to collect types, fileset and err
+	// use package parser to collect types, fileset and err
 	pkg, fset, err := pkgp(ctx, spkg)
 	if err != nil {
 		return nil, err
@@ -34,12 +36,37 @@ func NewPackageWalker(ctx context.Context, spkg string, pkgp Pkgp) (*Pkgw, error
 	return &Pkgw{fset: fset, pkg: pkg}, nil
 }
 
-// Visit is Pkgw implementation of Walker Visit
-// it goes through all struct decls inside the package
+// VisitTop implements Pkgw Walker VisitTop method
+// it goes through all top level struct decls inside the package
 // and applies strategy if struct name matches regexp
-func (pkgw Pkgw) Visit(ctx context.Context, reg *regexp.Regexp, sg Strategy) {
-	fset := pkgw.fset
+func (pkgw Pkgw) VisitTop(ctx context.Context, reg *regexp.Regexp, sg Strategy) {
 	s := pkgw.pkg.Scope()
+	pkgw.visit(ctx, reg, s, sg)
+}
+
+// VisitRec implements Pkgw Walker VisitRec method
+// it goes through all nested levels struct decls inside the package
+// and applies strategy if struct name matches regexp
+func (pkgw Pkgw) VisitRec(ctx context.Context, reg *regexp.Regexp, sg Strategy) {
+	// rec defines recursive function
+	// that goes through all nested scopes
+	var rec func(s *types.Scope)
+	rec = func(s *types.Scope) {
+		pkgw.visit(ctx, reg, s, sg)
+		for i := 0; i < s.NumChildren(); i++ {
+			chs := s.Child(i)
+			rec(chs)
+		}
+	}
+	s := pkgw.pkg.Scope()
+	rec(s)
+}
+
+// visit helps to implement Pkgw Walker VisitTop and VisitRec methods
+// it goes through all struct decls inside the scope
+// and applies strategy if struct name matches regexp
+func (pkgw Pkgw) visit(ctx context.Context, reg *regexp.Regexp, s *types.Scope, sg Strategy) {
+	fset := pkgw.fset
 	// go through all names inside the package scope
 	for _, name := range s.Names() {
 		// check if object name doesn't matches regexp
