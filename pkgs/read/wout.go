757e25f5f9b193128b9ef887f326e021e1158f0e
Kostiantyn Masliuk <alfa0dude@gmail.com>
2020-02-09 18:13:12 +0100

    fix parser issue with pattern name missmatch fix visiting structure name/type capturing fix conurent run in wout

--- pkgs/read/wout.go
+++ pkgs/read/wout.go
@@ -31,12 +31,12 @@ func (w wout) VisitDeep(ctx context.Context, regex *regexp.Regexp, stg gopium.St
 	return w.visit(ctx, regex, stg, true)
 }
 
-// visit wout helper that uses
+// visit wout helps with visiting and uses
 // gopium.Visit and gopium.VisitFunc helpers
 // to go through all structs decls inside the package
-// and apply strategy then get result
-// then use fmts.TypeFormat to format strategy result
-// and in the end use io.Writer to write output
+// and apply strategy to them to get results
+// then use fmts.TypeFormat to format strategy results
+// and use io.Writer to write results to output
 func (w wout) visit(ctx context.Context, regex *regexp.Regexp, stg gopium.Strategy, deep bool) error {
 	// check that formatter wasn't set correctly
 	if w.fmt == nil {
@@ -59,35 +59,49 @@ func (w wout) visit(ctx context.Context, regex *regexp.Regexp, stg gopium.Strate
 	visit := gopium.Visit(regex, stg, ch, deep)
 	// create separate cancelation context for visiting
 	nctx, cancel := context.WithCancel(ctx)
-	visit(nctx, tpkg.Scope())
+	// run visiting in separate goroutine
+	go visit(nctx, tpkg.Scope())
 	// go through results from visit func
-	for ster := range ch {
-		// in case any error happened in strategy
-		// cancel context and return error
-		if ster.Error != nil {
-			cancel()
-			return ster.Error
-		}
-		// apply formatter
-		buf, err := w.fmt(ster.Struct)
-		// in case any error happened in formatter
-		// cancel context and return error
-		if err != nil {
-			cancel()
-			return err
-		}
-		// apply writter
-		_, err = w.writer.Write(buf)
-		// in case any error happened in formatter
+	// we can use concurent writitng too
+	// but it's probably redundant
+	// as it requires additional level of sync
+	// and error handling
+	for sterr := range ch {
+		err := w.write(ctx, sterr)
+		// in case any error happened in writting
 		// cancel context and return error
 		if err != nil {
 			cancel()
 			return err
 		}
 	}
-	// we can sefely cancel context here
-	// as walk is done successfully
-	// and return nil error
+	// we can safely cancel context here
+	// as walk is already done successfully
+	// and returned nil error
 	cancel()
 	return nil
+}
+
+// visit wout helps apply
+// fmts.TypeFormat to format strategy result
+// and use io.Writer to write result to output
+// or return error in any other case
+func (w wout) write(ctx context.Context, sterr gopium.StructError) error {
+	// in case any error happened
+	// in strategy return error
+	if sterr.Error != nil {
+		return sterr.Error
+	}
+	// apply formatter
+	buf, err := w.fmt(sterr.Struct)
+	// in case any error happened
+	// in formatter and return error
+	if err != nil {
+		return err
+	}
+	// apply writter
+	_, err = w.writer.Write(buf)
+	// in case any error happened
+	// in writer return error
+	return err
 }
