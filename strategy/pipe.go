9aaf1ef549e32cc31afffc0f69d3b6fe317650c9
Kostiantyn Masliuk <1pkg@protonmail.com>
2020-03-24 22:33:56 +0100

    pipe stg update

--- strategy/pipe.go
+++ strategy/pipe.go
@@ -9,15 +9,13 @@ import (
 // pipe defines strategy implementation
 // that pipes together set of strategies
 // by applying them one after another
-type pipe struct {
-	stgs []gopium.Strategy
-}
+type pipe []gopium.Strategy
 
 // Apply pipe implementation
-func (stg pipe) Apply(ctx context.Context, o gopium.Struct) (r gopium.Struct, err error) {
+func (stgs pipe) Apply(ctx context.Context, o gopium.Struct) (r gopium.Struct, err error) {
 	// go through all inner strategies
 	// and apply them one by one
-	for _, stg := range stg.stgs {
+	for _, stg := range stgs {
 		r, err = stg.Apply(ctx, o)
 		// in case of any error
 		// return immediately
