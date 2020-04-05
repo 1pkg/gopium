6dce6e1edc5b5c52ea23c8eaaeef8e2f5f43a87f
Kostiantyn Masliuk <1pkg@protonmail.com>
2020-04-05 22:31:49 +0200

    simplify walker interface improve package layout

--- mocks/maven.go
+++ mocks/maven.go
@@ -12,7 +12,7 @@ type TypeMock struct {
 // MavenMock defines mock maven implementation
 type MavenMock struct {
 	Types                   map[string]TypeMock
-	Caches                  []int64
+	SysCacheVals            []int64
 	SysWordVal, SysAlignVal int64
 }
 
@@ -28,32 +28,40 @@ func (m MavenMock) SysAlign() int64 {
 
 // SysCache mock implementation
 func (m MavenMock) SysCache(level uint) int64 {
-	if int(level) < len(m.Caches) {
-		return m.Caches[level]
+	// check if we have it in vals
+	if int(level) < len(m.SysCacheVals) {
+		return m.SysCacheVals[level]
 	}
+	// otherwise return default val
 	return 0
 }
 
 // Name mock implementation
 func (m MavenMock) Name(t types.Type) string {
+	// check if we have it in vals
 	if t, ok := m.Types[t.String()]; ok {
 		return t.Name
 	}
+	// otherwise return default val
 	return ""
 }
 
 // Size mock implementation
 func (m MavenMock) Size(t types.Type) int64 {
+	// check if we have it in vals
 	if t, ok := m.Types[t.String()]; ok {
 		return t.Size
 	}
+	// otherwise return default val
 	return 0
 }
 
 // Align mock implementation
 func (m MavenMock) Align(t types.Type) int64 {
+	// check if we have it in vals
 	if t, ok := m.Types[t.String()]; ok {
 		return t.Align
 	}
+	// otherwise return default val
 	return 0
 }
--- walker.go
+++ walker.go
@@ -9,10 +9,7 @@ import (
 // that applies some strategy to tree structures
 // and modifies tree or creates other side effects
 type Walker interface {
-	// VisitTop visits only top level of the tree
-	VisitTop(context.Context, *regexp.Regexp, Strategy) error
-	// VisitDeep visits all levels of the tree
-	VisitDeep(context.Context, *regexp.Regexp, Strategy) error
+	Visit(context.Context, *regexp.Regexp, Strategy, bool) error
 }
 
 // WalkerName defines registered walker name abstraction
