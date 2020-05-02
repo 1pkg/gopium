61fd0c4e437e1c799cd48f2a094f22a0befd85d4
Kostiantyn Masliuk <alfa0dude@gmail.com>
2020-05-02 16:05:56 +0200

    add tests data parser cache and simplify visit tests

--- tests/data/parser.go
+++ tests/data/parser.go
@@ -6,18 +6,23 @@ import (
 	"go/parser"
 
 	"1pkg/gopium"
+	"1pkg/gopium/tests/data/cache"
 	"1pkg/gopium/typepkg"
 
 	"golang.org/x/tools/go/packages"
 )
 
+// p cache parser singleton
+var p cache.Parser
+
 // NewParser creates parser for single tests data
 func NewParser(pkg string) gopium.Parser {
-	return typepkg.ParserXToolPackagesAst{
+	parser := typepkg.ParserXToolPackagesAst{
 		Path:       fmt.Sprintf("%s/%s", "src/1pkg/gopium/tests/data", pkg),
 		Root:       build.Default.GOPATH,
 		ModeTypes:  packages.LoadAllSyntax,
 		ModeAst:    parser.ParseComments | parser.AllErrors,
 		BuildFlags: []string{"-tags=tests_data"},
 	}
+	return p.With(parser)
 }
