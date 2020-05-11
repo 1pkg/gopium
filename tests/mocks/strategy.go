c8b87084b9192cb460cadfa420ec60ffa6e1cd29
Kostiantyn Masliuk <1pkg@protonmail.com>
2020-05-11 22:06:39 +0200

    add tests coverage for runners move ctx pkg to root

--- tests/mocks/strategy.go
+++ tests/mocks/strategy.go
@@ -16,3 +16,14 @@ type Strategy struct {
 func (stg Strategy) Apply(context.Context, gopium.Struct) (gopium.Struct, error) {
 	return stg.R, stg.Err
 }
+
+// StrategyBuilder defines mock strategy builder implementation
+type StrategyBuilder struct {
+	Strategy gopium.Strategy
+	Err      error
+}
+
+// Build mock implementation
+func (b StrategyBuilder) Build(...gopium.StrategyName) (gopium.Strategy, error) {
+	return b.Strategy, b.Err
+}
