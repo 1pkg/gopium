f83a23b97085670b43e770028d0f57176523946d
Kostiantyn Masliuk <alfa0dude@gmail.com>
2020-05-18 20:47:28 +0200

    add vscode extension run facilities

--- cmd/vscode/src/extension.ts
+++ cmd/vscode/src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as conf from './confs';
+import run from "./run";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,11 +15,18 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('gopium.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from gopium!');
+	let disposable = vscode.commands.registerCommand('gopium.helloWorld', async () => {
+		const confs: conf.default = {
+			args: {
+				package: "1pkg/gopium",
+				walker: "json_file",
+				startegies: ["explicit_padings_type_natural", "add_tag_group_soft"],
+			} as conf.Args,
+			flags: {
+				w: 4,
+			} as conf.Flags,
+		}
+		await run(confs);
 	});
 
 	context.subscriptions.push(disposable);
