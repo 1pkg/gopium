a9f00693d8f99ecf31f11399e3b3ce01d2ca96c7
Kostiantyn Masliuk <1pkg@protonmail.com>
2020-05-20 22:19:30 +0200

    vendor vscode-go extension to use its facilities implement code-lens for gopium actions

--- cmd/vscode/src/extension.ts
+++ cmd/vscode/src/extension.ts
@@ -1,8 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import Gopiumcli from "./gopiumcli";
-import Workspace from "./workspace";
+import { GO_MODE } from '../vscode-go/src/goMode';
+import Codelens from './codelens';
+import Gopiumcli from './gopiumcli';
+import Workspace from './workspace';
 
 export interface Settings {
 	readonly presets: { [key: string]: Arguments };
@@ -42,18 +44,15 @@ export interface Runner {
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gopium" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('gopium.helloWorld', async () => {
-		let cli = new Gopiumcli();
-		await cli.run("test", new Workspace(), "", "");
+	let disposable = vscode.commands.registerCommand('gopium', async (preset, path, pkg, struct) => {
+		let gopiumcli = new Gopiumcli();
+		await gopiumcli.run(preset, new Workspace(), path, pkg, struct);
 	});
 
+	context.subscriptions.push(vscode.languages.registerCodeLensProvider(GO_MODE, new Codelens(new Workspace())));
 	context.subscriptions.push(disposable);
 }
 
