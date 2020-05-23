4c765bfccffe09da3b1ed29c824f64d5d95e8dee
Kostiantyn Masliuk <alfa0dude@gmail.com>
2020-05-23 20:52:31 +0200

    fix ts compiler config add pretier rules patch vscode-go functionality add install and full path gopium checks

--- cmd/vscode/src/gopiumcli.ts
+++ cmd/vscode/src/gopiumcli.ts
@@ -1,32 +1,46 @@
-import * as cp from 'child_process';
-import * as vscode from 'vscode';
-import * as extension from './extension';
+import * as cp from 'child_process'
+import { isAbsolute } from 'path'
+import * as vscode from 'vscode'
+import { promptForMissingTool } from '../vscode-go/src/goInstallTools'
+import { getBinPath } from '../vscode-go/src/util'
+import * as extension from './extension'
 
 export default class Gopiumcli implements extension.Runner {
-	async run(preset: string, settings: extension.Settings, path: string, pkg: string, struct?: string): Promise<boolean> {
+	async run(
+		preset: string,
+		settings: extension.Settings,
+		path: string,
+		pkg: string,
+		struct?: string,
+	): Promise<boolean> {
 		return new Promise<boolean>(async (resolve, reject) => {
 			// prepare out channel
-			const out = vscode.window.createOutputChannel('gopium');
-			out.clear();
-			out.show(true);
+			const out = vscode.window.createOutputChannel('gopium')
+			out.clear()
+			out.show(true)
 			// build the regex
-			let regex = /.*/;
+			let regex = /.*/
 			if (struct != null) {
-				regex = new RegExp(`^${struct}$`);
+				regex = new RegExp(`^${struct}$`)
 			}
-			// prepare final args and cmd
+			// prepare final args and cli
+			let gopium = getBinPath('gopium')
+			if (!isAbsolute(gopium)) {
+				console.log('AAAAA', gopium)
+				await promptForMissingTool('gopium')
+				gopium = getBinPath('gopium')
+			}
 			let args = settings.build(preset, path, pkg, regex.source)
-			let cmd = 'gopium ' + args.join(' ')
 			// start gopium process
-			out.appendLine(`executing gopium cli: ${cmd}`);
-			const proc = cp.spawn("gopium", args);
-			proc.stdout.on('data', (chunk) => out.append(chunk.toString()));
-			proc.stderr.on('data', (chunk) => out.append(chunk.toString()));
+			out.appendLine(`executing gopium cli: ${gopium} ${args.join(' ')}`)
+			const proc = cp.spawn(gopium, args)
+			proc.stdout.on('data', (chunk) => out.append(chunk.toString()))
+			proc.stderr.on('data', (chunk) => out.append(chunk.toString()))
 			proc.on('close', (code, signal) => {
-				out.appendLine("gopium cli finished execution");
+				out.appendLine('gopium cli finished execution')
 				// out.hide();
-				resolve(code === 0);
-			});
-		});
+				resolve(code === 0)
+			})
+		})
 	}
 }
