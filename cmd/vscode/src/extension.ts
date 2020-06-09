6fcdae92ea8cbc0208eae8f0becbd90de06b1f89
Kostiantyn Masliuk <1pkg@protonmail.com>
2020-06-10 00:44:26 +0200

    add vscode extension readme

--- cmd/vscode/src/extension.ts
+++ cmd/vscode/src/extension.ts
@@ -42,7 +42,7 @@ function patchgo() {
 		return [
 			{
 				name: 'gopium',
-				importPath: 'github.com/1pkg/gopim',
+				importPath: 'github.com/1pkg/gopim/cmd/gopim',
 				isImportant: true,
 				description: '',
 			},
@@ -60,7 +60,7 @@ function patchgo() {
 			case 'gopium':
 				return {
 					name: 'gopium',
-					importPath: 'github.com/1pkg/gopim',
+					importPath: 'github.com/1pkg/gopim/cmd/gopim',
 					isImportant: true,
 					description: '',
 				}
