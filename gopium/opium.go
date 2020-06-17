fb78341f237d3b6475346e593c90e3be6502593d
Kostiantyn Masliuk <1pkg@protonmail.com>
2020-06-17 22:50:02 +0200

    update version

--- gopium/opium.go
+++ gopium/opium.go
@@ -3,7 +3,7 @@ package gopium
 // list of global registered gopium constants
 const (
 	NAME    = "gopium"
-	VERSION = "1.0.1"
+	VERSION = "1.1.0"
 	PKG     = "https://github.com/1pkg/gopium"
 	STAMP   = "🌺 gopium @1pkg"
 )
