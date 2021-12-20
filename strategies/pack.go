f659e9db41de3f11c10b5b115751f1781b11f23b
Kostiantyn Masliuk <1pkg@protonmail.com>
2021-12-20 21:31:14 +0100

    update pack/unpack strategy to support ptr size sort

--- strategies/pack.go
+++ strategies/pack.go
@@ -25,12 +25,36 @@ func (stg pack) Apply(ctx context.Context, o gopium.Struct) (gopium.Struct, erro
 	// copy original structure to result
 	r := collections.CopyStruct(o)
 	// execute memory sorting
+	// https://cs.opensource.google/go/x/tools/+/refs/tags/v0.1.7:go/analysis/passes/fieldalignment/fieldalignment.go;l=145;bpv=0;bpt=1
 	sort.SliceStable(r.Fields, func(i, j int) bool {
-		// first compare aligns of two fields
+		// place zero sized objects before non-zero sized objects
+		zeroi, zeroj := r.Fields[i].Size == 0, r.Fields[j].Size == 0
+		if zeroi != zeroj {
+			return zeroi
+		}
+		// then compare aligns of two fields
 		// bigger aligmnet means upper position
 		if r.Fields[i].Align != r.Fields[j].Align {
 			return r.Fields[i].Align > r.Fields[j].Align
 		}
+		// place pointerful objects before pointer-free objects
+		noptri, noptrj := r.Fields[i].Ptr == 0, r.Fields[j].Ptr == 0
+		if noptri != noptrj {
+			return noptrj
+		}
+
+		if !noptri {
+			// if both have pointers
+			// then place objects with less trailing
+			// non-pointer bytes earlier;
+			// that is, place the field with the most trailing
+			// non-pointer bytes at the end of the pointerful section
+			traili, trailj := r.Fields[i].Size-r.Fields[i].Ptr, r.Fields[j].Size-r.Fields[j].Ptr
+			if traili != trailj {
+				return traili > trailj
+			}
+		}
+
 		// then compare sizes of two fields
 		// bigger size means upper position
 		return r.Fields[i].Size > r.Fields[j].Size
